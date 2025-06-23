e67f353352250a497c191a8918f40edabe971ba9
Geison Brando <geisondante@gmail.com>
2025-06-23 02:08:28 -0500

    adding content in the new Navi branch

--- combustivel.py
+++ combustivel.py
@@ -1,5 +1,5 @@
-km = float(input())
-l = float(input())
+km = float(input("insira o valor de km rodados: "))
+l = float(input("insira a quantidade de galosina por litro foram gastos: "))
 consMedio = km / l
 casaDec = round(consMedio, 3)
 print(casaDec, "km/l")
